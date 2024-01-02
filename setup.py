78d174dad5eee76a6d5164789004e998ffe26d32
Gustavo S. Teixeira <gsteixei@gmail.com>
2024-01-02 14:32:51 -0300

    project rename (#2)

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-encrypted-file-field",
-    version="0.1.0",
+    name="django-filefield-encrypted",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     license="GNU License",
     description="Encryted file field for Django",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/gsteixeira/django-encrypted-file-field",
+    url="https://github.com/gsteixeira/django-filefield-encrypted",
     author="Gustavo Selbach Teixeira",
     author_email="gsteixei@gmail.com",
     zip_safe=False,
